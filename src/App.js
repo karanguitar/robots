59c16170e75ee4b52fb1ad4d0ee5bb24759fc53a
Karan Dhanoya <kdhanoya94@gmail.com>
2018-11-17 13:36:43 +0000

    adding redux

--- src/App.js
+++ src/App.js
@@ -1,39 +1,59 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
 import CardList from './components/robots/CardList'
-import {robots} from './robots'
 import SearchBar from './components/searchbar/SearchBar'
+import Scroll from './components/util/Scroll'
+import {setSearchField, requestRobots} from './actions/actions'
 
 class App extends Component {
 
-    state={
-        robots: robots,
-        searchfield: ""
+    componentWillMount(){
+        this.props.onRequestRobots()
     }
-
-    handleChange = (event) =>{
-
-        this.setState({
-            searchfield: event.target.value
-        })
-
-    }
-
 
 
     render() {
-        const filteredRobots = this.state.robots.filter((robot) =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const {searchField, handleChange, robots, isPending} = this.props
+
+        const filteredRobots = robots.filter((robot) =>{
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
 
-        return (
+        return isPending ?
+           <div>Loading</div> : 
             
+            (
+        
             <div className="tc">
                 <h1>RoboFriends</h1>
-                <SearchBar handleChange={this.handleChange}/>
-                <CardList robots={filteredRobots}/>
+                <SearchBar handleChange={handleChange}/>
+                <Scroll>
+                { filteredRobots.length >=1 ? <CardList robots={filteredRobots}/> : <h2 className="red"> {searchField} does not exist</h2> }
+                </Scroll>
             </div>
         );
     }
 }
 
-export default App;
+const mapStateToProps = (state) =>{
+    return{
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error,
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+   return {
+       handleChange: (event) => dispatch(setSearchField(event.target.value)),
+       onRequestRobots: () => dispatch(requestRobots())
+       //we are getting the action and setting it up in this container 
+    }
+}
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
